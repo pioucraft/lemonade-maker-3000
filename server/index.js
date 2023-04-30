0192d9c612c9e507b219268e886c4fd1c0933da0
pfannkuchen <nathan.pioucraft@outlook.com>
2023-04-30 19:12:27 +0200

    started the project

--- server/index.js
+++ server/index.js
@@ -1,2 +1,23 @@
-console.log("wowwwww")
+const express = require("express")
+let app = express()
+const port = 3000
 
+const mongoose = require("mongoose")
+const User = require("./User")
+
+const async = require("async")
+const crypto = require("crypto")
+
+
+const registerQueue = async.queue(async (task) => {
+    task.req.params.password = crypto.createHash
+})
+
+app.all("/api/register/:username/:pasword", (req, res) => {
+    registerQueue.push({req, res})
+})
+
+
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`)
+})
